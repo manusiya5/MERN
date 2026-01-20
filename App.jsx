d6651db6f630f947804a9dd2f374a243388a7087
Anusiya M <137895245+manusiya5@users.noreply.github.com>
2026-01-20 14:51:13 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,40 +1,16 @@
-
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Card from './components/Card'
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 import './App.css'
-import Navbar from './components/Navbar'
-import Courses from './components/Courses'
-function App() {
 
+function App() {
 
   return (
     <>
-      <div>
-      
-        <Header />
-        <h3>Hi , This is the app content.</h3>
-        <p> React app content can be added here.</p>
-        <Courses/>
-        <div className='flex flex-wrap'>
-        <Card cardTitle="React Js Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Full Stack Java Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="MEAN Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="MERN Course" cardDescription="Module frontend course for 40 days"></Card>
-         <Card cardTitle="Dev OPs Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Cloud Computing Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Dot Net Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Python Course" cardDescription="Module frontend course for 40 days"></Card>
-          <Card cardTitle="Azure  Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Angular Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Data Analyst Course" cardDescription="Module frontend course for 40 days"></Card>
-        <Card cardTitle="Data Science Course" cardDescription="Module frontend course for 40 days"></Card>
-        </div>
-       
-        <Footer />
-        
-      </div>
-      
+     <h1 className='bg-black'>come</h1>
+     <Header/>
+     <Menu/>
+     <Footer/>
     </>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,28 @@
-import Navbar from "./Navbar";
-function Header()
-{
-    function get_brand_name()
-    {
-        return "Learning React ";
-    }
-    let brand_name = "Great Learning";
-return(
-<div className=" p-5 border-b m-2 border-gray-300 w-full  px-2 py-5  bg-neutral-900 text-amber-50">
-  <Navbar/>
-    <h1 className="text-2xl font-bold underline">Welcome to react basics</h1>
+import React from "react";
 
-    <p className="font-light"> Wow , we are welcoming you on this website we have created</p>
-    <h1> Brand Name : {brand_name}</h1>
-       <h1> Topic : {get_brand_name()}</h1>
+
+function Header() {
+return (
+<header className="bg-black text-white px-6 py-4">
+<div className="flex items-center justify-between">
+<div className="flex items-center gap-2">
+<div className="w-10 h-10 bg-white text-orange-600 font-bold rounded-full flex items-center justify-center">
+🍗
+</div>
 </div>
 
-)
+
+<h1 className="text-2xl font-bold absolute left-1/2 transform -translate-x-1/2">
+ Biriyani Shop
+</h1>
+
+
+<nav className="flex gap-6 text-lg font-medium">
+<a href="#" className="hover:text-orange-200">Home</a>
+<a href="#" className="hover:text-orange-200">Menu</a>
+</nav>
+</div>
+</header>
+);
 }
 export default Header;
